1f040c64c18242dcd337fb8d47744baf6985bf38
sunodmongia <sunodmongia2003@gmail.com>
2024-09-21 13:52:22 +0530

    NOT NULL constraint failed: error solved

---
+++ customer_management/mixin.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.mixins import AccessMixin
+from django.shortcuts import redirect
+
+
+class OrganiserLoginRequiredMixin(AccessMixin):
+    """Verify that the current user is authenticated and is an organiser"""
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated or not request.user.is_organiser:
+            return redirect('lead_list')
+        return super().dispatch(request, *args, **kwargs)
