0f2e3ad9d4318027450503e56051d1e6cac02645
sunodmongia <sunodmongia2003@gmail.com>
2024-09-28 09:53:49 +0530

    updated

--- CRM_project/settings.py
+++ CRM_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = "django-insecure-e#j7ol+0ul!m0j$qlo&k7o!ms^@v79j#ta#(h9=x1op*#cee9h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,13 +138,15 @@ LOGIN_URL = "/login"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Email settings
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("WIRE_APP_EMAIL_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("WIRE_APP_EMAIL_PASSWORD")
+
+
 
 # Security settings
 SESSION_COOKIE_SECURE = True
